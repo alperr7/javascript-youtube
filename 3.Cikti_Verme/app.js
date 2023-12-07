8529f4e76ba737c58bef8c7c85f08c1fa0b8069d
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-07 19:47:19 +0300

    l

---
+++ 3.Cikti_Verme/app.js
@@ -0,0 +1,41 @@
+/* Document write ile çıktı verme */ 
+
+// document.writeln("Hello World <br>");
+// document.writeln("Alper Kandemir<br>");
+// document.write(5.5);
+
+
+/* console log metodunu kullanırız  */
+
+// console.log("Hello World");
+// console.log("Alper Kandemir");
+// console.log(3.53);
+// console.log(true);
+// console.log({name:"Alper",surname:"Kandemir"});
+
+// console.clear();
+
+// let a=5;
+
+// let b=10;
+
+// console.log("İki sayının toplamı : " , (a+b));
+
+// console.clear();
+
+
+/* Alert (Uyarı) popup ile çıktı verme işlemleri  */
+
+// let a = 10;
+// let b = 12;
+
+
+// alert("F5 tuşuna lütfen basmayınız");
+
+
+console.log(window);
+console.error("Hata oluştu");
+console.log(location.port);
+
+
+
