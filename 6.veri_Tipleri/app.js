eb0396dd540722bb589a133de7ce509f3f0a57e0
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-18 22:13:51 +0300

    l

---
+++ 6.veri_Tipleri/app.js
@@ -0,0 +1,33 @@
+/*
+? Veri Tipleri
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+
+*/
+
+// let isim = "Alper Kandemir 20 yaşındadır.";
+// console.log(isim);
+// console.log(typeof isim);
+
+// let sayi1 = 10;
+
+// console.log(sayi1);
+// console.log(typeof sayi1);
+
+let a = "5";
+console.clear();
+console.log(typeof a);
+
+let stringDegisken = "Yaşınız : ";
+let yas = 23; 
+
+console.log(stringDegisken + yas);
+
+// boolean : true ve false'dan oluşur
+console.log(5>8);
