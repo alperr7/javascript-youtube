534f213e1327867286cb0d0f1738f4e3a5a42a26
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-19 22:47:43 +0300

    l

---
+++ 7.aritmetik_operatorler/app.js
@@ -0,0 +1,57 @@
+// ARİTMETİK OPERATÖRLER 
+
+/* ------------------>  = atama operatörü önemli ! 
++
+-
+*
+%
+++
+--
+**
+
+
+
+*/
+
+// string + number      stringnumber
+// let not1 = 60;
+// let not2 = 70;
+
+// console.log(not1-not2);
+
+// let sayi1 = "5";
+// let sayi2 = 2;
+
+// console.log(sayi1 + sayi2);
+
+// console.log("Notlarınızın Toplamı : " + (not1 + not2));
+
+// let sayi1 = 5;
+// let sayi2 = 10;
+ 
+// console.log(sayi1 * sayi2);
+
+
+//ü,ö,ş,İ,ğ
+//birleşik olarak tanımlanmalıdır değişken ismi.
+
+// let musteriNo = 5;
+// let sayi2 = 10;
+
+// let not1 = 100;
+// let not2 = 70;
+
+// let sonuc = (not1+not2)/2;
+// console.log(sonuc);
+
+
+// % : mod alma operatörü 
+/** 
+ 
+    10/3 = 1;
+    12/5 = 2;
+  
+ */
+
+console.log(12%5);
+
