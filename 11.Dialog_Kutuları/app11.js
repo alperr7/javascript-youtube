e954353d5016143d735c5b802bbd51d373fb2d2f
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-21 14:49:20 +0300

    l

---
+++ 11.Dialog_Kutuları/app11.js
@@ -0,0 +1,18 @@
+/* 
+
+-------------DİYALOG KUTULARI ----------
+
+    -Alert
+    -Prompt
+    -Confirm
+*/
+
+// console.log(window);
+
+//function
+// alert("İşleminize devam etmeden önce kaydetmelisiniz");
+
+//Prompt();  Kullanıcıdan değer alabiliyoruz... 
+
+let isim = prompt("İsminizi giriniz : ");
+console.log(isim);
