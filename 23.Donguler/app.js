23f91d059fc090dbaa27ab6f86bac40985d64f8a
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-26 12:20:32 +0300

    l

--- 23.Donguler/app.js
+++ 23.Donguler/app.js
@@ -6,14 +6,49 @@
 // }
 
 //1 den 10 a kadar çiftleri yazdıralım
-for(let i=0;i<=10;i++){
-    if(i%2==0){
-        console.log(i);
-    }
-}
+// for(let i=0;i<=10;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
 
 //1 den 10 a kadar tekleri yazdıralım
-for(let i=1;i<=10;i+=2){
-    console.log(i);
+// for(let i=1;i<=10;i+=2){
+//     console.log(i);
+// }
+
+/**
+ Enes
+ Naber
+ Enes
+ Naber 
+ */
+
+//  for(let i=0;i<=10;i++){
+//     if(i%2==0){
+//         console.log("Alper");
+//     }else{
+//         console.log("Naber");
+//     }
+//  }
+
+//50 den 1 e kadar gidelim ve toplamlarını bulalım 
+// let toplam=0;
+// for(let i=50;i>=1;i--){
+//     toplam+=i;
+    
+// }
+
+// console.log("Toplam : " + toplam);
+
+
+/*---------------------------------------*/
+
+// 1 den 10 ' a kadar yazdıralım 
+let sayac=1;
+while(sayac<=10){
+    console.log(sayac);
+    sayac++;
 }
+
 
