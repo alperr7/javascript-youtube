8d6ed2cab490f555464d11c4d6f9eb436cabb036
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2024-01-02 10:01:46 +0300

    l

---
+++ 36-DateSinifKullanımı/app.js
@@ -0,0 +1,25 @@
+//DATE Kullanımı 
+
+let tarih = new Date();
+// console.log(tarih);
+// console.log(tarih.getFullYear());
+// console.log(tarih.getDate());
+// console.log(tarih.getDay());//haftanın kaçıncı gününde olduğunu söyler
+// console.log(tarih.getHours());
+// console.log(tarih.getMilliseconds());
+// console.log(tarih.getMinutes());
+// console.log(tarih.getMonth()+1);
+// console.log(tarih.getSeconds());
+// console.log(tarih.toLocaleDateString());
+// console.log(tarih.toLocaleTimeString());
+// console.log(tarih.toLocaleString());
+
+//SET METOTLARI 
+console.log(tarih);
+// tarih.setHours(15);
+// tarih.setMonth(11);
+// tarih.setMinutes(50);
+// tarih.setDate(24);
+
+// tarih.setHours(tarih.getHours()+2);
+console.log(tarih);
---
+++ 37-DegerVeReferansTipler/app.js
@@ -0,0 +1,34 @@
+//DEĞER VE REFERANS TİPLER 
+
+// let a = 5;
+
+// let b = a;
+
+// console.log("a : " + a);
+// console.log("b : " + b);
+
+// console.log("----------------------------");
+
+// b=10;
+// console.log("a : " + a);
+// console.log("b : " + b);
+
+// let dizi1 = [1,2,3];
+// let dizi2 = [1,2,3];
+
+// if(dizi1==dizi2){
+//     console.log("eşittir");
+// }else{
+//     console.log("eşit değildir");
+// }
+// console.log(dizi1);
+// console.log(dizi2);
+
+let dizi1= [1,2,3];
+let dizi2 = dizi1;
+let dizi3 = dizi2;
+
+dizi2.push(26);
+console.log(dizi1);
+console.log(dizi2);
+console.log(dizi3);
